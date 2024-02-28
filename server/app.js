082d638059ffbf8193c21e42f34f6cb78d21b50d
morotioye <morotioye@gmail.com>
2024-02-28 14:32:36 -0500

    fix: migrated back to https - required for google SSO

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const http = require('http'); // Using HTTP instead of HTTPS
+const https = require('https');
+const fs = require('fs'); 
 
 const app = express();
 
@@ -9,11 +10,21 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
 
+
+
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
+
+const privateKeyPath = process.env.PRIVATE_KEY_PATH;
+const certificatePath = process.env.CERTIFICATE_PATH;
+
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
 
 // MongoDB connection using Mongoose
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -29,7 +40,7 @@ app.use(express.json()); // For parsing application/json
 const userRoutes = require('./routes/users');
 app.use('/users', userRoutes);
 
-// Start server using HTTP
-http.createServer(app).listen(PORT, () => {
-    console.log(`HTTP Server running on port ${PORT}`);
+// Start server
+https.createServer(credentials, app).listen(PORT, () => {
+    console.log(`HTTPS Server running on port ${PORT}`);
 });
