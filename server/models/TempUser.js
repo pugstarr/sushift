7af166bfb1eeace5c203eac98546a774dba5feeb
morotioye <morotioye@gmail.com>
2024-03-02 23:10:06 -0500

    backend model change

--- server/models/TempUser.js
+++ server/models/TempUser.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 
 const tempUserSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  availability: {
+    monday: { type: String, enum: ['none', 'morning', 'night', 'full'], default: 'none' },
+    tuesday: { type: String, enum: ['none', 'morning', 'night', 'full'], default: 'none' },
+    wednesday: { type: String, enum: ['none', 'morning', 'night', 'full'], default: 'none' },
+    thursday: { type: String, enum: ['none', 'morning', 'night', 'full'], default: 'none' },
+    friday: { type: String, enum: ['none', 'morning', 'night', 'full'], default: 'none' },
+    saturday: { type: String, enum: ['none', 'morning', 'night', 'full'], default: 'none' },
+    sunday: { type: String, enum: ['none', 'morning', 'night', 'full'], default: 'none' },
+  },
 });
 
 module.exports = mongoose.model('TempUser', tempUserSchema);
